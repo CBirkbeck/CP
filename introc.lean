cd9d3407de70a855ad7ce6dcaefed3ac1a0aa7de
CBirkbeck <c.birkbeck@ucl.ac.uk>
2022-10-28 15:19:29 +0100

    dgh45

---
+++ introc.lean
@@ -0,0 +1,14 @@
+/-
+# CP grand challenge
+
+## By Chris Birkbeck 
+
+
+
+# Challenge 
+
+Solve the levels and I'll buy you all beer/coffee/whatever you can drink.
+
+
+
+-/
---
+++ tutorialc.lean
@@ -0,0 +1,25 @@
+import tactic --hide
+
+
+/-
+
+#Goldbach
+
+-/
+
+
+/- Lemma
+If $n$ is even then there exits primes $p,q$ such that $n = p+q$.
+-/
+lemma Goldbach (n : ℕ) (hn : even n) : ∃ (p q : ℕ) (hp : prime p) (hq : prime q), n = p + q :=
+begin
+  sorry,
+
+
+
+
+
+
+end
+
+
